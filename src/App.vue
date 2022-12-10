f099a4cd4cb734900a5a8ee3faff0b0113fce066
Bril3d <khaledtbk@gmail.com>
2022-12-10 17:39:09 +0100

    Storing Favourites

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="main">
-    <button @click="fetchData">Fetch Another Image</button>
+    <div>
+      <button @click="fetchData">Fetch Another Image</button>
+      <button @click="saveCat">Save It</button>
+    </div>
+
       <div v-if="(imgs && loading)" class="card">
         <img :src="imgs.url" :alt="imgs.source_url">
         <h1>{{imgs.artist}}</h1>
       </div>
+      
       <div v-else>
         <h1>Loading...</h1>
       </div>
+      
   </div>
+  <CatBoysCollection :stored="this.stored"></CatBoysCollection>
 </template>
 <style>
 body {
@@ -30,28 +37,33 @@ font-family: sans-serif;
 img {
   object-fit: cover;
   width: 100%;
-  height: 100%;
+  height: 80%;
 }
 button{
 padding: 5px 10px;
 border-radius: 5px;
 background-color: white;
 color: red;
-margin: 50px 0;
+margin: 50px 10px;
 width: fit-content;
 cursor: pointer;
+
 }
 </style>
 <script>
+import CatBoysCollection from "./components/CatBoysCollection.vue";
 export default {
   data() {
     return {
       imgs:null,
-      loading:false
+      loading:false,
+      stored:[],
     }
     
   },
-
+  components:{
+    CatBoysCollection
+},
 methods: {
   async fetchData(){
     this.loading = false;
@@ -62,9 +74,17 @@ methods: {
       this.loading = true;
     },1000)
     
+  },
+  saveCat(){
+    this.stored.push(this.imgs)
+    localStorage.setItem('CatBoys', JSON.stringify(this.stored));
   }
 },
 mounted() {
+
+if(localStorage.CatBoys){
+  this.stored = JSON.parse(localStorage.getItem("CatBoys"));
+};
 this.fetchData()
 }
 }
---
+++ src/components/CatBoysCollection.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="collection">
+    <div v-for="Cat in storedCats" class="container">
+       
+            <img :src="Cat.url" :alt="Cat.source_url">
+            <h5>{{ Cat.artist }}</h5>
+        
+        
+    </div>
+    </div>
+</template>
+<style scoped>
+    .collection{
+        display: grid;
+        grid-template-columns: repeat( auto-fill, minmax(200px, 1fr) );
+        grid-template-rows: repeat( auto-fill, minmax(250px, 1fr) );
+        gap:10px;
+    }
+    .container{
+    width: 200px;
+    height: 200px;
+    color: red;
+    text-align: center;
+    }
+
+</style>
+<script>
+export default {
+    props:{
+        stored:Array
+    },
+    computed: {
+        storedCats() {
+            return JSON.parse(JSON.stringify(this.stored));
+        }
+    }
+
+}
+
+</script>
